0ce0842dd7553302d2b4003782778309c8ac9ee8
Derek <derekb0147@gmail.com>
2020-04-17 23:44:50 -0400

    change withdraw and deposit amount to default to 0 avoiding int and none type error

--- Stocker/.history/site_users/models_20200416220725.py
+++ Stocker/.history/site_users/models_20200416220725.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.apps import apps
 from django.contrib.auth.models import AbstractUser
 from portfolio.models import Portfolio
-from decimal import Decimal
 
 class Custom_User(AbstractUser):
     email = models.EmailField()
@@ -11,7 +10,7 @@ class Custom_User(AbstractUser):
     last_name = models.CharField(max_length=42)
     
     # in pennies, not dollars
-    deposits = models.DecimalField(decimal_places=2, max_digits=8, null=True , default=Decimal('0.00'))
-    withdraws = models.DecimalField(decimal_places=2, max_digits=8, null=True, default=Decimal('0.00'))
+    deposits = models.DecimalField(decimal_places=2, max_digits=8, null=True)
+    withdraws = models.DecimalField(decimal_places=2, max_digits=8, null=True)
 
     favorites = models.CharField(max_length=599)
