23f797cd25bf741fd0b902963fa01e855aa38678
Imraj423 <dahqniss@gmail.com>
2020-04-18 14:07:28 -0400

    remove history from repo

---
+++ .venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/daku/Desktop/capstone1/Stocker-Beta/.venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- Stocker/.history/site_users/tests_20200418120309.py
+++
@@ -1,5 +0,0 @@
-from django.test import TestCase
-from .helpers import *
-
-class HelperTestCase(TestCase):
-    pass
