a3017050c950a2a63e0210cd4b8738f059b3f9d2
Tamoya Hopkins <tamoya@250ok.com>
2020-04-20 14:50:33 -0400

    made API tokens in helper file secret using new .env file, be sure to poetry install after you merge

--- Stocker/site_users/helpers.py
+++ Stocker/site_users/helpers.py
@@ -1,21 +1,29 @@
 import requests
 from portfolio.models import Company
 
+# add secret api keys
+import os
+from dotenv import load_dotenv
+from Stocker.settings import BASE_DIR
+load_dotenv(dotenv_path=f'{BASE_DIR}/Stocker/.env')
+
 def fetchCompanyData(ticker):
-    r = requests.get(f'https://cloud.iexapis.com/stable/stock/{ticker}/company?token=pk_8b849948242d4bb38c80005cdf53f2a6')
+    key = os.getenv("IEX_KEY_1")
+    r = requests.get(f'https://cloud.iexapis.com/stable/stock/{ticker}/company?token={key}')
     return r.json()
 
 
 def fetchTicker(ticker):
-    r = requests.get(f'https://sandbox.iexapis.com/stable/stock/{ticker}/quote?token=Tpk_40ebc98b6181404fac1a900fa92b4690')
+    key = os.getenv("IEX_KEY_2")
+    r = requests.get(f'https://sandbox.iexapis.com/stable/stock/{ticker}/quote?token={key}')
 
     co = Company.objects.filter(ticker_symbol=ticker).first()
     if co:
         co.price = r.json()['latestPrice']
         co.save()
     else:
-        Company.objects.create(name=r.json()['companyName'], 
-                               ticker_symbol=r.json()['symbol'], 
+        Company.objects.create(name=r.json()['companyName'],
+                               ticker_symbol=r.json()['symbol'],
                                price=r.json()['latestPrice'])
 
     return r.json()
@@ -23,7 +31,7 @@ def fetchTicker(ticker):
 
 def multiFetcher(follow_list):
     stock_tickers = []
-    for tkr in follow_list:        
+    for tkr in follow_list:
         data = fetchTicker(tkr)
         stock_tickers.append({
             'symbol': tkr,
